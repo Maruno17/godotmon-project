1d4b59d2d4098f53c2da90e4c0fa2e72b1606f83
Luximora <luximoragames@gmail.com>
2025-02-27 11:09:48 -0500

    Update main.gd
    Added a static instance

--- src/main/main.gd
+++ src/main/main.gd
@@ -1,10 +1,14 @@
 extends Node
 class_name Main
 
-
 ## The main screen of the project. Every single world, entity and UI element will be a child of one of the nodes of this scene.[br]
 ## Scenes can be added or removed from it using the ScenesManager autoload.
 
-
 @onready var world_parent: Node2D = $WorldParent
 @onready var ui_parent: Control = $UiParent
+
+## Allows you to reference this node kinda like an autoload by just doing Main.instance.whatever_you_want_here()
+static var instance:Main
+
+func _init():
+	instance = self
